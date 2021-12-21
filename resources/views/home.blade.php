ede8b73b9813779ee6ad7ebb0195e427181f09e0
vindya147 <wattegedaravindya147@gmail.com>
2021-12-21 13:35:33 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="app.css" rel="stylesheet" />
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+    <title>Oogway's Adventures</title>
+
+</head>
+
+
+<body class="bg-light">
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white shadow">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Oogway's Adventures</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="#">Add New</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5" style="max-width: 55rem;">
+
+        <!-- Main Post -->
+        <div class="row mb-5">
+            <div class="col-md-12 py-3">
+
+                <div class="card shadow-sm rounded-3">
+
+                    <a class="list-group-item p-0 border-0" href="/">
+
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src="#" class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h2 class="card-title">Lorem Ipsum 6</h2>
+                                    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                        Repudiandae vel optio, ad in delectus, mollitia ab quibusdam sint dolorem
+                                        debitis ut
+                                        doloremque dignissimos assumenda recusandae incidunt beatae, alias quam. Optio?
+                                    </p>
+                                    <button class="btn btn-sm btn-outline-dark rounded-pill px-3 mb-2"><a href="/posts/post6">Read
+                                        Article</button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+
+                </div>
+            </div>
+
+            <!-- Middle Posts -->
+            <div class="col-md-6 py-3">
+
+                <div class="card shadow-sm rounded-3">
+
+                    <a class="list-group-item p-0 border-0" href="/post.html">
+
+                        <img class="card-img-top img-responsive" src="#" alt="card-img">
+                        <div class="card-body">
+                            <h3 class="card-title">Lorem Ipsum 01</h3>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+                                maiores
+                                ipsam id fuga eaque, necessitatibus tempora officiis doloribus quam vel cumque minus hic
+                                corporis! Ipsam amet eius quasi est explicabo!</p>
+                            <button class="btn btn-sm btn-outline-dark rounded-pill px-3 mb-2"><a href="/posts/post1">Read Article</button>
+                        </div>
+
+                    </a>
+
+                </div>
+
+            </div>
+
+            <div class="col-md-6 py-3">
+
+                <div class="card shadow-sm rounded-3">
+
+                    <a class="list-group-item p-0 border-0" href="/">
+
+                        <img class="card-img-top img-responsive" src="#" alt="card-img">
+                        <div class="card-body">
+                            <h3 class="card-title">Lorem Ipsum 02</h3>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+                                maiores
+                                ipsam id fuga eaque, necessitatibus tempora officiis doloribus quam vel cumque minus hic
+                                corporis! Ipsam amet eius quasi est explicabo!</p>
+                            <button class="btn btn-sm btn-outline-dark rounded-pill px-3 mb-2"><a href="/posts/post2">Read Article</button>
+                        </div>
+
+                    </a>
+
+                </div>
+
+            </div>
+
+            <!-- Bottom Posts -->
+            <div class="col-md-4 py-3">
+
+                <div class="card shadow-sm rounded-3">
+
+                    <a class="list-group-item p-0 border-0" href="/">
+
+                        <img class="card-img-top img-responsive" src="#" alt="card-img">
+                        <div class="card-body">
+                            <h4 class="card-title">Lorem Ipsum 03</h4>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+                                maiores
+                                ipsam id fuga eaque, necessitatibus tempora officiis doloribus quam vel cumque minus hic
+                                corporis! Ipsam amet eius quasi est explicabo!</p>
+                            <button class="btn btn-sm btn-outline-dark rounded-pill px-3 mb-2 text-center"><a href="/post/post3">Read
+                                Article</button>
+                        </div>
+                    </a>
+
+                </div>
+
+            </div>
+
+            <div class="col-md-4 py-3">
+
+                <div class="card shadow-sm rounded-3">
+
+                    <a class="list-group-item p-0 border-0" href="/">
+
+                        <img class="card-img-top img-responsive" src="#" alt="card-img">
+                        <div class="card-body">
+                            <h4 class="card-title">Lorem Ipsum 04</h4>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+                                maiores
+                                ipsam id fuga eaque, necessitatibus tempora officiis doloribus quam vel cumque minus hic
+                                corporis! Ipsam amet eius quasi est explicabo!</p>
+                            <button class="btn btn-sm btn-outline-dark rounded-pill px-3 mb-2"><a href="/posts/post4">Read Article</button>
+                        </div>
+
+                    </a>
+
+                </div>
+
+            </div>
+
+            <div class="col-md-4 py-3">
+
+                <div class="card shadow-sm rounded-3">
+
+                    <a class="list-group-item p-0 border-0" href="/">
+
+                        <img class="card-img-top img-responsive" src="#" alt="card-img">
+                        <div class="card-body">
+                            <h4 class="card-title">Lorem Ipsum 05</h4>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+                                maiores
+                                ipsam id fuga eaque, necessitatibus tempora officiis doloribus quam vel cumque minus hic
+                                corporis! Ipsam amet eius quasi est explicabo!</p>
+                            <button class="btn btn-sm btn-outline-dark rounded-pill px-3 mb-2"><a href="/posts/post5">Read Article</button>
+                        </div>
+
+                    </a>
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('posts/{post}',function($slug){
+    $path=__DIR__ . "/../resources/posts/{$slug}.html";
+    if(! file_exists($path)){
+        return redirect('/');
+    }
+    $post=file_get_contents($path);
+    return view('post',[
+        'post'=> $post
+    ]);
+   
+});
